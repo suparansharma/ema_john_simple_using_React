552e6936a4036d34f4d86969cfb9f45990fd38d9
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-20 02:41:37 +0600

    using router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import SHOP from './components/SHOP/SHOP';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import Nomatch from './components/Nomatch/Nomatch';
+
 
 function App() {
   return (
     <div>
-    <Header></Header>
-    <SHOP></SHOP>
+      <Header></Header>
+      <Router>
+        <Switch>
+        <Route path="/shop">
+        <SHOP></SHOP>
+        </Route>
+        <Route path="/review">
+        <Review></Review>
+        </Route>
+        <Route path="/manage">
+        <Inventory></Inventory>
+        </Route>
+        <Route exact path="/">
+        <SHOP></SHOP>
+        </Route>
+        <Route path="*">
+        <Nomatch></Nomatch>
+        </Route>
+        </Switch>
+      </Router>
+    
+   
     </div>
   );
 }
---
+++ src/components/Nomatch/Nomatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nomatch = () => {
+    return (
+        <div>
+            <h1>404 this page not found</h1>
+        </div>
+    );
+};
+
+export default Nomatch;
