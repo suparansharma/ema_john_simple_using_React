f1e21e4c0748e6f53932147da24e5a36d410fc8d
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-22 16:05:49 +0600

    remove item

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart,removeFromDatabaseCart } from '../../utilities/databaseManager';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
 
     const[ cart,setCart]=useState([]);
+
+    const removeProduct = (productKey) =>{
+
+        console.log('remove click',productKey);
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+         removeFromDatabaseCart(productKey);
+       
+    }
+
 
     useEffect(()=>{
 
@@ -28,6 +38,7 @@ const Review = () => {
            
            cart.map(pd => <ReviewItem 
             key={pd.key}
+            removeProduct={removeProduct}
             product={pd}></ReviewItem>)
            
            }
