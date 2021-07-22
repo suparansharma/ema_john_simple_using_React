f516fc35ba7f8405b380eaec77245347d6733535
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-23 01:24:14 +0600

    dataload system

--- src/components/SHOP/SHOP.js
+++ src/components/SHOP/SHOP.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import fakedata from '../../fakeData';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart,getDatabaseCart } from '../../utilities/databaseManager';
 import CART from '../CART/CART';
 import PRODUCT from '../PRODUCT/PRODUCT';
 import './SHOP.css'
@@ -11,12 +11,42 @@ const SHOP = () => {
     const [products,setProducts] = useState(first10);
     const [cart,setCart] = useState([]);
 
+    useEffect(()=>{
+
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const previousCart = productKeys.map(existingKey => {
+            const product = fakedata.find(pd => pd.key ===existingKey )
+            product.quantity = savedCart[existingKey];
+            return product;
+            
+        })
+        setCart(previousCart);
+    },[])
+
     const handleAddProduct = (product)=>{
-        console.log('product add',product);
-        const newCart = [...cart,product];
+    const toBeAddedKey = product.key;
+    const sameProduct = cart.find(pd =>pd.key=== toBeAddedKey);
+    let count = 1;
+    let newCart;
+    if(sameProduct){
+
+        count = sameProduct.quantity + 1;
+        sameProduct.quantity = count + 1;
+        const others = cart.filter(pd => pd.key !== toBeAddedKey);
+        newCart = [...others,sameProduct]
+
+    }
+
+    else{
+
+        product.quantity = 1;
+         newCart = [...cart,product]
+    }
+        // const count = sameProduct.length;
+        // const newCart = [...cart,product];
         setCart(newCart);
-        const sameProduct = newCart.filter(pd =>pd.key=== product.key);
-        const count = sameProduct.length;
+        
         addToDatabaseCart(product.key,count);
 
     }
